b0eb763942fbf12be866562767c9d628afe3875a
ForAnyMove <alex.osadchuk.98@gmail.com>
2024-05-22 15:17:32 +0200

    crm post hook

--- index.js
+++ index.js
@@ -13,16 +13,23 @@ formList.forEach(formElement =>
     };
     
     var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://b24-ydta3n.bitrix24.eu/rest/1/i2i7zsa5gssywfv3/', true);
+    xhr.open('POST', 'https://b24-ydta3n.bitrix24.eu/rest/1/i2i7zsa5gssywfv3/crm.lead.add', true);
     xhr.setRequestHeader('Content-Type', 'application/json');
     
     xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            alert('Заявка успешно отправлена!');
-            document.getElementById('leadForm').reset();
-        }
+      if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+              alert('Заявка успешно отправлена!');
+              document.getElementById('leadForm').reset();
+          } else {
+              console.error('Ошибка при отправке заявки: ' + xhr.status + ' ' + xhr.statusText);
+              console.error('Response: ' + xhr.responseText);
+              alert('Произошла ошибка при отправке заявки. Пожалуйста, попробуйте еще раз.');
+          }
+      }
     };
-    
+
+    console.log(JSON.stringify(data));
     xhr.send(JSON.stringify(data));
   })
 )
